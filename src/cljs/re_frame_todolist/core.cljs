cd469b47429e645948f197ef1a8b6c7a3901d8f5
sandmark <sandmark.m@gmail.com>
2019-11-08 22:15:36 +0900

    Init

---
+++ src/cljs/re_frame_todolist/core.cljs
@@ -0,0 +1,23 @@
+(ns re-frame-todolist.core
+  (:require
+   [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
+   [re-frame-todolist.events :as events]
+   [re-frame-todolist.views :as views]
+   [re-frame-todolist.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app")))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
