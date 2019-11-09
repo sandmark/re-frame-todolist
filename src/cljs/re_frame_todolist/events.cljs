a78b0f7e5254c5f408c9571c1c3565faf72765b3
sandmark <sandmark.m@gmail.com>
2019-11-09 16:25:20 +0900

    Add feature `new todo`

--- src/cljs/re_frame_todolist/events.cljs
+++ src/cljs/re_frame_todolist/events.cljs
@@ -1,10 +1,17 @@
 (ns re-frame-todolist.events
-  (:require
-   [re-frame.core :as re-frame]
-   [re-frame-todolist.db :as db]
-   ))
+  (:require [re-frame.core :as re-frame]
+            [re-frame-todolist.db :as db]))
 
 (re-frame/reg-event-db
  ::initialize-db
  (fn [_ _]
    db/default-db))
+
+(defn- allocate-next-id [todos]
+  (-> todos keys last ((fnil inc 0))))
+
+(re-frame/reg-event-db
+ ::add-todo
+ (fn [db [_ title]]
+   (let [id (allocate-next-id (:todos db))]
+     (assoc-in db [:todos id] {:id id :title title}))))
--- src/cljs/re_frame_todolist/views.cljs
+++ src/cljs/re_frame_todolist/views.cljs
@@ -1,13 +1,31 @@
 (ns re-frame-todolist.views
   (:require [re-frame.core :as re-frame]
-            [re-frame-todolist.subs :as subs]))
+            [re-frame-todolist.subs :as subs]
+            [reagent.core :as reagent]
+            [clojure.string :as str]
+            [re-frame-todolist.events :as events]))
 
 (defn todo-item [todo]
   [:li (:title todo)])
 
+(defn todo-input []
+  (let [!val (reagent/atom "")]
+    (fn []
+      [:input.new-todo {:type        "text"
+                        :value       @!val
+                        :placeholder "What needs to be done?"
+                        :on-change   #(reset! !val (-> % .-target .-value))
+                        :on-key-down #(when (= 13 (.-which %))
+                                        (let [title (str/trim @!val)]
+                                          (when (seq title)
+                                            (re-frame/dispatch [::events/add-todo title]))
+                                          (reset! !val "")))}])))
+
 (defn todo-list []
   (let [todos @(re-frame/subscribe [::subs/todos])]
-    [:ul
-     (for [todo todos]
-       ^{:key (:id todo)}
-       [todo-item todo])]))
+    [:div
+     [todo-input]
+     [:ul
+      (for [todo todos]
+        ^{:key (:id todo)}
+        [todo-item todo])]]))
